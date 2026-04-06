626974e9e62fa914c9da0aad2c29a688c2e100b1
Amineba45 <mouhamadoulamineba52@gmail.com>
2026-04-06 19:32:36 +0000

    Add userController with basic CRUD operations - 2026-04-06 19:30:30

---
+++ src/controllers/userController.js
@@ -0,0 +1,19 @@
+// User Controller
+
+const getUserProfile = (userId) => {
+    // Logic to get user profile
+};
+
+const updateUserProfile = (userId, updatedData) => {
+    // Logic to update user profile
+};
+
+const getAllUsers = () => {
+    // Logic to get all users
+};
+
+const deleteUser = (userId) => {
+    // Logic to delete a user
+};
+
+module.exports = { getUserProfile, updateUserProfile, getAllUsers, deleteUser };
